715911f6cca67e84503ffb65857e29e420121aaf
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 09:10:01 +0700

    js lab 26

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+let num1 = Number(prompt("Input your number 1 : "));
+let num2 = Number(prompt("Input your number 2 : "));
+let num3 = Number(prompt("Input your number 3 : "));
+let numArray = [num1, num2, num3];
+let sortNum = numArray.sort((a, b) => b - a);
+alert(sortNum);
